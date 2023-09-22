4ed49fe4567ef191128835b573ea063c77e80dbb
sharif ahmad <sharif.ahmad@agoda.com>
2023-09-22 15:15:11 +0700

    some work

--- ktor-web-applications-start/src/main/kotlin/org/applications/plugins/Templating.kt
+++ ktor-web-applications-start/src/main/kotlin/org/applications/plugins/Templating.kt
@@ -6,6 +6,8 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.Thymeleaf
 import io.ktor.server.thymeleaf.ThymeleafContent
+import org.applications.model.Priority
+import org.applications.model.Task
 import org.applications.model.TaskRepository
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
@@ -36,6 +38,21 @@ fun Application.configureTemplating() {
                     return@get
                 }
                 call.respond(ThymeleafContent("single-task", mapOf("task" to task)))
+            }
+            get("byPriority") {
+                try {
+                    val priority: Priority = Priority.valueOf(call.request.queryParameters["priority"]!!)
+                    val tasks: List<Task> = TaskRepository.tasksByPriority(priority = priority)
+                    if (tasks.isEmpty()) {
+                        val message = "There are no tasks with priority $priority"
+                        call.respond(ThymeleafContent("error", mapOf("message" to message)))
+                    }
+                }
+                catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+
             }
         }
     }
